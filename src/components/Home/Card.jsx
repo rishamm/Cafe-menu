ec66828abfe00588aa7279484d01425254005498
rishamm <muhammedrisham45334@gmail.com>
2023-12-11 14:38:07 +0530

    0.1

---
+++ src/components/Home/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className="card ">
+    <h6>coffee</h6>
+
+  </div>
+  )
+}
+
+export default Card
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 import "./home.css";
+import Banner from "../components/Home/Banner";
+import Card from "../components/Home/Card";
 const Home = () => {
   return (
     <>
-      <div className="container mx-auto flex justify-center menubgimg">
-        <h1 className="heddinggg self-center ">Menu</h1>
-      </div> 
+      <Banner/>
+      <Card/>
 
-      <div className="card ">
-        <h6>coffee</h6>
-
-      </div>
     </>
   );
 };
